92a48edf201b991688c1f9bec2be112fb20bd1cd
LucaGuazzelli <116031604+LucaGuazzelli@users.noreply.github.com>
2022-11-29 14:49:20 -0300

    nova interface

---
+++ Interfaceid.py
@@ -0,0 +1,57 @@
+
+
+
+import pygame, sys
+
+def Bola_animacao():
+	global Bola_vel_x, Bola_vel_y
+	
+	Bola .x += Bola_vel_x
+	Bola .y += Bola_vel_y
+
+	if Bola .top <= 0 or Bola .bottom >= Tela_altura:
+		Bola_vel_y *= -1
+	if Bola .left <= 0 or Bola .right >= Tela_largura:
+		Bola_vel_x *= -1
+
+	if Bola.colliderect(Jogador) or Bola.colliderect(Adversario):
+		Bola_vel_x *= -1
+
+
+pygame.init()
+clock = pygame.time.Clock()
+
+Tela_largura = 1200
+Tela_altura = 500
+Tela = pygame.display.set_mode((Tela_largura,Tela_altura))
+pygame.display.set_caption('Jogo.Pygame - Luca, PH, Rafa')
+
+
+Cor = (200,200,200)
+bg_color = pygame.Color('grey12')
+
+
+
+Bola  = pygame.Rect(Tela_largura / 2 - 15, Tela_altura / 2 - 15, 30, 30)
+Jogador = pygame.Rect(Tela_largura - 20, Tela_altura / 2 - 70, 10,140)
+Adversario = pygame.Rect(10, Tela_altura / 2 - 70, 10,140)
+
+Bola_vel_x = 7
+Bola_vel_y = 7
+
+while True:
+	for event in pygame.event.get():
+		if event.type == pygame.QUIT:
+			pygame.quit()
+			sys.exit()
+	
+	Bola_animacao()
+
+	Tela.fill('grey12')
+	pygame.draw.rect(Tela, Cor, Jogador)
+	pygame.draw.rect(Tela, Cor, Adversario)
+	pygame.draw.ellipse(Tela, Cor, Bola )
+	pygame.draw.aaline(Tela, Cor, (Tela_largura / 2, 0),(Tela_largura / 2, Tela_altura))
+
+	pygame.display.flip()
+	clock.tick(60)
