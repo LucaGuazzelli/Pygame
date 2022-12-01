3a95dd9fc9b78efdd3142ec52b82e260ff8e2b70
LucaGuazzelli <116031604+LucaGuazzelli@users.noreply.github.com>
2022-12-01 14:02:24 -0300

    coments

--- interface-V2.py
+++ interface-V2.py
@@ -5,12 +5,16 @@ from pygame import mixer
 from playsound import playsound
 import os
 
+
 pygame.init()
 Tempo_pontos = True
 Jogador_pontos = 0 
 Adversario_pontos = 0 
+#impotanado as fontes dos pontos 
 fonte = pygame.font.SysFont("Free Sans Bold", 32)
 tela_inicio = pygame.image.load('assets/tela1.jpeg')
+
+#animacao da bola e para ela bater nas paredes 
 
 def Bola_animacao():
 	global Bola_vel_x, Bola_vel_y,Tempo_pontos,Adversario_pontos, Jogador_pontos
@@ -38,14 +42,14 @@ def Bola_animacao():
 		elif abs(Bola.bottom - Jogador.top) < 10 or abs(Bola.top - Jogador.bottom) < 10 :
 			Bola_vel_y *= -1
 			
-		#play_sound(sons['som1'])	
+	
 
 	if Bola.colliderect(Adversario):
 		if abs(Bola.left - Adversario.right) < 10 or abs(Bola.right - Jogador.left) < 10:
 			Bola_vel_x *= -1	
 		else:
 			Bola_vel_y *= -1
-
+#def do jogador 
 def player_animation():
 	Jogador.y += jogador_Vel
 
@@ -54,6 +58,7 @@ def player_animation():
 	if Jogador.bottom >= Tela_altura:
 		Jogador.bottom = Tela_altura 
 
+#def que faz o ai exister e jogar contra o player 
 def adversario():
 	if Adversario.top < Bola.y:
 		Adversario.y += Adversario_vel
@@ -64,7 +69,7 @@ def adversario():
 		Adversario.top = 0
 	if Adversario.bottom >= Tela_altura:
 		Adversario.bottom = Tela_altura
-
+#def para fazer a bola comecar no lugar e comecar a bola e sua diercao 
 def bola_start():
 	global Bola_vel_y, Bola_vel_x, Bola_mex, Tempo_pontos, Cor
 
@@ -95,19 +100,23 @@ def menu(tela):
 	return None
 
 clock = pygame.time.Clock()
+#size da tela 
 Tela_largura = 1200
 Tela_altura = 500
 Tela = pygame.display.set_mode((Tela_largura,Tela_altura))
 pygame.display.set_caption('Jogo.Pygame - Luca, PH, Rafa')
 
+
+#variaveis de cada cor que vao ser usadas depois
 Cor = (200,200,200)
 bg_color = pygame.Color('grey12')
 
-
+#lugar de cada item na tela do jogo
 Bola  = pygame.Rect(Tela_largura / 2 - 15, Tela_altura / 2 - 15, 30, 30)
 Jogador = pygame.Rect(Tela_largura - 20, Tela_altura / 2 - 70, 10,140)
 Adversario = pygame.Rect(10, Tela_altura / 2 - 70, 10,140)
 
+#variaveis
 Bola_vel_x = 7 * random.choice((1,-1))
 Bola_vel_y = 7 * random.choice((1,-1))
 jogador_Vel = 0
@@ -115,7 +124,6 @@ Adversario_vel = 7
 Bola_mex = False 
 
 pygame.init() 
-
 pygame.mixer.pre_init(44100, -16, 1, 512)
 pygame.mixer.init()
 pygame.mixer.music.load("musica1.wav")
@@ -124,11 +132,12 @@ menu_inicial = True
 
 while True:
 	jogo_start = False 
+	#para fachar o programa
 	for event in pygame.event.get():
 		if event.type == pygame.QUIT:
 			pygame.quit()
 			sys.exit()
-
+		#comando que o jogador faz
 		if event.type == pygame.KEYDOWN:
 			if event.key == pygame.K_UP:
 					jogador_Vel -= 7
@@ -140,9 +149,10 @@ while True:
 				if event.key == pygame.K_DOWN:
 					jogador_Vel -= 7
 
-
+		#Jogo tocar musica 
 		pygame.mixer.music.play()
 		pygame.display.update()
+	#Jogo stop depois de 6 pontos 
 	if Jogador_pontos > 5 or Adversario_pontos > 5:
 		pygame.quit()
 	
@@ -154,6 +164,7 @@ while True:
 	player_animation()
 	adversario()
 
+	#para criara os item que estao no jogo 
 	Tela.fill('grey12')
 	pygame.draw.rect(Tela, Cor, Jogador)
 	pygame.draw.rect(Tela, Cor, Adversario)
@@ -163,7 +174,7 @@ while True:
 
 	if Tempo_pontos:
 		bola_start()
-	
+	#para escrver os pontos
 	player_text = fonte.render(f'{Jogador_pontos}',False,Cor)
 	Tela.blit(player_text,(1000,47))
 
